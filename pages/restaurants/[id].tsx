085a514e7e1d085639f901859be8de05de3a0f2e
sushilburagute <codetastic101@gmail.com>
2021-12-27 19:41:14 +0530

    Add: Navbar + SEO to restaurant page

--- pages/restaurants/[id].tsx
+++ pages/restaurants/[id].tsx
@@ -1,13 +1,19 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 
-export const RestaurantPage: NextPage = () => {
+import { Navbar, Layout, Jumbotron, SortBar, Card, SEO } from "./../../components/index";
+
+const RestaurantPage: NextPage = () => {
   const router = useRouter();
   const { pid } = router.query;
   return (
     <>
+      <SEO title="Restaurants | Zuck" />
+      <Navbar />
       <h1>Restaurant Page</h1>
       <p>Product: {pid}</p>
     </>
   );
 };
+
+export default RestaurantPage;
